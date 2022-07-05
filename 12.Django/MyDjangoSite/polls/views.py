45989692236b7e377581d7eee164e6fb43b88cce
Bekalu <beckpostgrad@gmail.com>
2022-07-05 18:23:47 +0200

    django back end update area of c

--- 12.Django/MyDjangoSite/polls/views.py
+++ 12.Django/MyDjangoSite/polls/views.py
@@ -9,7 +9,7 @@ from django.http import HttpResponse
 def indexPolls(request):
 
     
-    return render(request, "home.html", {"name":"BUddy"})
+    return render(request, "home.html", {"name":"now"})
 
 
 def add(request):
@@ -17,7 +17,7 @@ def add(request):
     num1 = float(request.GET["number1"])
     num2 = float(request.GET["number2"])
 
-    rez = num1 / num2
+    rez = num1 * (num2**2)
 
     return render(request, "result.html", {"sum":rez})
 
